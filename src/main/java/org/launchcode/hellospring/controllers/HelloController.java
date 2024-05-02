5a1e454992162fda5abbded8b14bce5d5649c11c
nicolepeak <colecpeak@gmail.com>
2024-05-02 10:22:21 -0500

    learning thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,57 +2,83 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+//@RequestMapping("hello")
 public class HelloController {
 
-    //now lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
+    @RequestMapping(value= "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
-    @GetMapping("form")
-    public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action= 'hello' method='post'>" + //submit request to /hello
-                "<input type='text' name='name' placeholder='Enter your name'>" +
-                "<select name = 'language'>" +
-                "<option value='french'>french</option>" +
-                "<option value='english'>english</option>" +
-                "<option value='arabic'>arabic</option>" +
-                "<option value='italian'>italian</option>" +
-                "<option value='spanish'>spanish</option>" +
-                "</select>" +
-                "<input type='submit' value ='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    public static String createMessage(String name, String language) {
-        if (language.equals("french")) {
-            return "bonjour, " + name + "!";
-        } else if (language.equals("english")) {
-            return "Hello, " + name + "!";
-        } else if (language.equals("arabic")) {
-            return "Marhaba, " + name + "!";
-        } else if (language.equals("italian")) {
-            return "bonjourno, " + name + "!";
-        } else return "Hola, " + name + "!";
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Cole");
+        names.add("Cole2");
+        names.add("Cole3");
+        model.addAttribute("names", names); //first is the value the template sees, and the second is the value that variable should have
+        return "hello-list";
     }
 
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
 
-        return createMessage(name, language);
-    }
+
+//    //now lives at /hello/goodbye
+//    @GetMapping("goodbye")
+//    @ResponseBody
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+//
+//    @GetMapping("form")
+//    public String helloForm(){
+//        return "form";
+//    }
+
+//    public static String createMessage(String name, String language) {
+//       String languageGreeting;
+//        if (language.equals("french")) {
+//            languageGreeting = "Bonjour";
+//        } else if (language.equals("english")) {
+//            languageGreeting = "Hello";
+//        } else if (language.equals("arabic")) {
+//            languageGreeting = "Marhaba";
+//        } else if (language.equals("italian")) {
+//           languageGreeting = "Bonjorno";
+//        } else {
+//          languageGreeting = "Hola";
+//        }
+//        return "languageGreeting";
+//    }
+//
+//    @RequestMapping(value="hello", method = RequestMethod.POST)
+//    public String helloPost(@RequestParam String name, @RequestParam String language, Model model) {
+//        if (name == null) {
+//            name = "World";
+//        }
+//
+//        String message = createMessage(name, language);
+//        model.addAttribute("languageGreeting", message); // Corrected: message is an object
+//        model.addAttribute("name", name); // Corrected: name is an object
+//
+//        return "languageGreeting";
+//    }
+
+
 
 }
